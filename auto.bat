bf5e4641cc310c8903d44605e143dce8361a42d0
dady_ocat@hotmail.com <dady_ocat@hotmail.com>
2023-11-14 18:26:45 +0700

    Auto Backup 2023 1Tu.182645

--- auto.bat
+++ auto.bat
@@ -4,5 +4,5 @@ git add --all
 git commit -am "Auto Backup %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
 git push
 echo Auto Backup %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%
-timeout /t 60
+timeout /t 5
 goto x
